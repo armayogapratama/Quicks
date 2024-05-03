001033f8474aa31dc0b586857f6b01c0c38671ff
Arma Yoga Pratama <mgarmayogapratama@gmail.com>
2024-05-03 13:52:40 +0700

    Feat: Home View

---
+++ client-side/quicks/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import HomeView from "./components/HomeViews/homeView";
+
+function App() {
+  return (
+    <>
+      <HomeView />
+    </>
+  );
+}
+
+export default App;
---
+++ client-side/quicks/src/components/HomeViews/homeView.jsx
@@ -0,0 +1,49 @@
+import { TfiBoltAlt } from "react-icons/tfi";
+import { GoSearch } from "react-icons/go";
+import { PiChatsDuotone } from "react-icons/pi";
+import { FaTasks } from "react-icons/fa";
+
+export default function HomeView() {
+  return (
+    <>
+      <div className="flex flex-row gap-[.1rem]">
+        <div className="bg-[#4f4f4f] w-[13rem] h-[50.8rem]"></div>
+
+        <div className="bg-[#4f4f4f] w-full">
+          <div>
+            <input
+              className="bg-[#828282] rounded w-full h-[2.5rem]"
+              type="text"
+              name="search"
+            />
+            <GoSearch className="relative top-[-1.8rem] left-[1rem] text-white" />
+          </div>
+
+          <div className="flex flex-row]">
+            <button className="flex justify-center items-center relative bottom-[-43.5rem] right-[-75rem] bg-[#2f80ed] w-[2.5rem] h-[2.5rem] rounded-[50%] cursor-pointer">
+              <TfiBoltAlt className="text-white text-2xl" />
+            </button>
+
+            <div className="flex flex-col">
+              <h3 className="flex text-white text-[14px] justify-center items-center relative bottom-[-42rem] right-[-69rem]">
+                Chats
+              </h3>
+              <button className="flex justify-center items-center relative bottom-[-42.2rem] right-[-69rem] bg-[#ffffff] w-[2.5rem] h-[2.5rem] rounded-[50%] cursor-pointer">
+                <PiChatsDuotone className="text-[#2f80ed] text-xl" />
+              </button>
+            </div>
+
+            <div className="flex flex-col">
+              <h3 className="flex text-white text-[14px] justify-center items-center relative bottom-[-42rem] right-[-63rem]">
+                Tasks
+              </h3>
+              <button className="flex justify-center items-center relative bottom-[-42.2rem] right-[-63rem] bg-[#ffffff] w-[2.5rem] h-[2.5rem] rounded-[50%] cursor-pointer">
+                <FaTasks className="text-[#f8b76b] text-lg" />
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
