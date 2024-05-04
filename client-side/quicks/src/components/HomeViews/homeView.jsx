9cf96bd09ef0dcc7647faf36307186b5261bcf23
Arma Yoga Pratama <mgarmayogapratama@gmail.com>
2024-05-04 16:24:31 +0700

    Feat: Task View Styling

--- client-side/quicks/src/components/HomeViews/homeView.jsx
+++ client-side/quicks/src/components/HomeViews/homeView.jsx
@@ -5,8 +5,11 @@ import { TfiBoltAlt } from "react-icons/tfi";
 import { GoSearch } from "react-icons/go";
 import { PiChatsDuotone } from "react-icons/pi";
 import { FaTasks } from "react-icons/fa";
+import { useState } from "react";
 
 export default function HomeView() {
+  const [active, setActive] = useState(false);
+
   return (
     <>
       <div className="flex flex-row gap-[.1rem]">
@@ -30,29 +33,41 @@ export default function HomeView() {
             <TaskView />
           </div>
 
-          <div className="flex flex-row]">
-            <button className="flex justify-center items-center relative bottom-[-43.5rem] right-[-75rem] bg-[#2f80ed] w-[2.5rem] h-[2.5rem] rounded-[50%] cursor-pointer">
-              <TfiBoltAlt className="text-white text-2xl" />
-            </button>
-
-            <div className="flex flex-col">
-              <h3 className="flex text-white text-[14px] justify-center items-center relative bottom-[-42rem] right-[-69rem]">
-                Chats
-              </h3>
-              <button className="flex justify-center items-center relative bottom-[-42.2rem] right-[-69rem] bg-[#ffffff] w-[2.5rem] h-[2.5rem] rounded-[50%] cursor-pointer">
-                <PiChatsDuotone className="text-[#2f80ed] text-xl" />
+          {!active ? (
+            <div className="flex flex-row]">
+              <button
+                onClick={() => setActive(true)}
+                className="flex justify-center items-center relative bottom-[-43.5rem] right-[-75rem] bg-[#2f80ed] w-[2.5rem] h-[2.5rem] rounded-[50%] cursor-pointer">
+                <TfiBoltAlt className="text-white text-2xl" />
               </button>
             </div>
-
-            <div className="flex flex-col">
-              <h3 className="flex text-white text-[14px] justify-center items-center relative bottom-[-42rem] right-[-63rem]">
-                Tasks
-              </h3>
-              <button className="flex justify-center items-center relative bottom-[-42.2rem] right-[-63rem] bg-[#ffffff] w-[2.5rem] h-[2.5rem] rounded-[50%] cursor-pointer">
-                <FaTasks className="text-[#f8b76b] text-lg" />
+          ) : (
+            <div className="flex flex-row]">
+              <button
+                onClick={() => setActive(false)}
+                className="flex justify-center items-center relative bottom-[-43.5rem] right-[-75rem] bg-[#2f80ed] w-[2.5rem] h-[2.5rem] rounded-[50%] cursor-pointer">
+                <TfiBoltAlt className="text-white text-2xl" />
               </button>
+
+              <div className="flex flex-col">
+                <h3 className="flex text-white text-[14px] justify-center items-center relative bottom-[-42rem] right-[-69rem]">
+                  Chats
+                </h3>
+                <button className="flex justify-center items-center relative bottom-[-42.2rem] right-[-69rem] bg-[#ffffff] w-[2.5rem] h-[2.5rem] rounded-[50%] cursor-pointer">
+                  <PiChatsDuotone className="text-[#2f80ed] text-xl" />
+                </button>
+              </div>
+
+              <div className="flex flex-col">
+                <h3 className="flex text-white text-[14px] justify-center items-center relative bottom-[-42rem] right-[-63rem]">
+                  Tasks
+                </h3>
+                <button className="flex justify-center items-center relative bottom-[-42.2rem] right-[-63rem] bg-[#ffffff] w-[2.5rem] h-[2.5rem] rounded-[50%] cursor-pointer">
+                  <FaTasks className="text-[#f8b76b] text-lg" />
+                </button>
+              </div>
             </div>
-          </div>
+          )}
         </div>
       </div>
     </>
--- client-side/quicks/src/components/TaskViews/taskView.jsx
+++ client-side/quicks/src/components/TaskViews/taskView.jsx
@@ -1,6 +1,9 @@
 import { FaRegUser } from "react-icons/fa6";
 import { FaAngleDown } from "react-icons/fa";
 import { FaAngleUp } from "react-icons/fa";
+import { BsThreeDots } from "react-icons/bs";
+import { MdOutlineWatchLater } from "react-icons/md";
+import { GoPencil } from "react-icons/go";
 import { useState } from "react";
 
 export default function TaskView() {
@@ -33,43 +36,64 @@ export default function TaskView() {
             </div>
           )}
 
-          <div className="flex bg-[#2f80ed] rounded px-2 w-[7rem] h-[2.5rem]">
+          <div className="flex justify-center items-center hover:outline hover:outline-2 hover:outline-[#828282] hover:shadow-md bg-[#2f80ed] rounded px-2 w-[7rem] h-[2.5rem]">
             <button className="text-white">New Task</button>
           </div>
         </div>
 
         <div className="overflow-x-scroll my-[22px] mx-[32px]">
-          <div className="flex flex-row gap-1 border-b pb-[22px] border-b-black">
+          <div className="flex flex-row gap-1 mb-[22px] border-b pb-[22px] border-b-black">
             <div className="flex flex-row">
-              <div className="flex flex-row justify-center items-center bg-[#e0e0e0] w-[2.5rem] h-[2.5rem] rounded-full">
-                <FaRegUser className="text-[#4f4f4f]" />
-              </div>
-
-              <div className="flex flex-row justify-center items-center relative left-[-1.3rem] bg-[#2f80ed] w-[2.5rem] h-[2.5rem] rounded-full">
-                <FaRegUser className="text-white" />
+              <div className="flex flex-row justify-center mt-[.3rem] mr-[20px]">
+                <input
+                  className="size-4 bg-[#4f4f4f] text-[#4f4f4f] border-2 border-[#4f4f4f] cursor-pointer"
+                  type="checkbox"
+                  name="tasks"
+                />
               </div>
             </div>
 
             <div className="flex flex-col">
               <div className="flex flex-row gap-4 w-full">
-                <div className="flex w-[80%]">
+                <div className="flex w-[70%]">
                   <h3 className="text-[#2f80ed]">
                     Update My Application For Ready To Deploy
                   </h3>
                 </div>
 
-                <div className="flex justify-end w-[10rem]">
+                <div className="flex w-[20rem] gap-3 absolute right-[-2rem]">
+                  <h3 className="text-[#eb5757] text-[14px]">2 Days Left</h3>
+
                   <h3 className="text-[#4f4f4f] opacity-65 text-[14px]">
                     May 3,2024 19.48
                   </h3>
+
+                  <div className="flex flex-row justify-center] gap-2">
+                    <FaAngleUp className="text-[#4f4f4f]" />
+
+                    <BsThreeDots className="text-[#4f4f4f]" />
+                  </div>
                 </div>
               </div>
 
-              <h3 className="text-[14px] font-semibold">Ellen :</h3>
-              <p className="text-[12px]">
-                Please check your whatsapp to get my message, because it is very
-                important!
-              </p>
+              <div className="flex flex-row gap-4 items-center my-[14px]">
+                <MdOutlineWatchLater className="text-[#2f80ed]" />
+
+                <input
+                  className="border border-[#4f4f4f] cursor-pointer rounded p-[0.2rem] w-[10rem]"
+                  type="date"
+                  name="date"
+                />
+              </div>
+
+              <div className="flex flex-row gap-4 items-center">
+                <GoPencil className="text-[#2f80ed]" />
+
+                <p className="text-[14px] w-[80%]">
+                  Please check your whatsapp to get my message, because it is
+                  very important!
+                </p>
+              </div>
             </div>
           </div>
         </div>
